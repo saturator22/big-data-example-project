4ddbada3ac6cd165b1146f5ca0983df7ebabffa9
saturator22 <f.szczeski@gmail.com>
2019-01-22 15:39:22 +0100

    Adjust App class to project requirements

--- project/src/main/java/gd/App.java
+++ project/src/main/java/gd/App.java
@@ -1,13 +1,25 @@
 package gd;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import gd.EventFactory.EventFactory;
+import gd.EventModelStructure.Event;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class App {
+    public static void main( String[] args ) throws FileNotFoundException {
+        EventFactory eventFactory = new EventFactory();
+
+        PrintWriter printWriter = new PrintWriter(new File("test.csv"));
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < 10 ; i++) {
+            Event event = eventFactory.getRandomEvent();
+            sb.append(event.toString());
+        }
+
+        printWriter.write(sb.toString());
+        printWriter.close();
     }
 }
