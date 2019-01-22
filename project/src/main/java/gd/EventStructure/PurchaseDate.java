bc31e622a8de0f6af3e4f8ea4e00ced26700945c
saturator22 <f.szczeski@gmail.com>
2019-01-22 15:34:11 +0100

    Implement PurchaseDate helper class

---
+++ project/src/main/java/gd/EventStructure/PurchaseDate.java
@@ -0,0 +1,30 @@
+package gd.EventStructure;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+public class PurchaseDate {
+
+    private LocalDateTime date;
+
+    public PurchaseDate() {
+        this.date = generateDate();
+    }
+
+    private LocalDateTime generateDate() {
+        Random random = new Random();
+        int randomDay = random.nextInt(8) + 1;
+        int randomHour = random.nextInt(23) + 1;
+        int randomMinute = random.nextInt(59) + 1;
+        int randomSecond = random.nextInt(59) + 1;
+
+        return LocalDateTime.of(2019, 7, randomDay, randomHour, randomMinute, randomSecond);
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return date.format(formatter);
+    }
+}
