44373cf3e653e71237b58c1f5f8ec739b6a2b97d
saturator22 <f.szczeski@gmail.com>
2019-01-24 17:43:07 +0100

    Adjust event producer to send test 10 events

--- project/src/main/java/gd/App.java
+++ project/src/main/java/gd/App.java
@@ -1,25 +1,34 @@
 package gd;
 
 import gd.EventFactory.EventFactory;
-import gd.EventModelStructure.Event;
+import gd.EventModelStructure.EventModel;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.Socket;
 
 public class App {
-    public static void main( String[] args ) throws FileNotFoundException {
-        EventFactory eventFactory = new EventFactory();
+    public static void main( String[] args ) throws IOException {
 
-        PrintWriter printWriter = new PrintWriter(new File("test.csv"));
+        String ip = "192.168.130.131";
+        int port = 56565;
+
+        Socket socket = new Socket(ip, port); // Create and connect the socket
+        DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+        PrintWriter pw = new PrintWriter(dOut);
+
+        EventFactory eventFactory = new EventFactory();
         StringBuilder sb = new StringBuilder();
 
         for(int i = 0; i < 10 ; i++) {
-            Event event = eventFactory.getRandomEvent();
-            sb.append(event.toString());
+            EventModel eventModel = eventFactory.getRandomEvent();
+            sb.append(eventModel.toString());
+            System.out.println(eventModel.toString());
         }
 
-        printWriter.write(sb.toString());
-        printWriter.close();
+        pw.write(sb.toString());
+        pw.flush();
+        pw.close();
     }
 }
